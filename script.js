2e4f4501b76850696c8d98bd26d8754f32f751b5
Adailson <adailson65@gmail.com>
2025-01-14 19:58:01 -0300

    first commit

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function playSound(soundId) {
     sound.currentTime = 0; // Reseta o áudio para começar do início
     sound.play();
 }
+
+
+// Captura o controle de cor
+const corControl = document.getElementById("corbody");
+
+// Função para alterar a cor do fundo com base no valor do range
+corControl.addEventListener("input", function () {
+    const corValue = corControl.value;
+    const color = `rgb(${255 - (corValue * 2.55)}, ${255 - (corValue * 2.55)}, ${255 - (corValue * 2.55)})`;
+    document.body.style.backgroundColor = color;
+});
+
+
+
+// Captura o controle de cor para a div.mpc-body
+const corMpcControl = document.getElementById("cormpc");
+
+corMpcControl.addEventListener("input", function () {
+    const corValue = corMpcControl.value;
+    const color = `rgb(${255 - (corValue * 2.55)}, ${255 - (corValue * 2.55)}, ${255 - (corValue * 2.55)})`;
+    const mpcBody = document.querySelector('.mpc-body');
+    mpcBody.style.backgroundColor = color;
+});
